c7f8b75c33fa1093765d5d66212f50e93ea08d02
Zannatul-Naim <zannatulnaim09@gmail.com>
2022-01-17 03:20:33 +0600

    A - not

---
+++ Atcoder/ABC/ABC178/A.cpp
@@ -0,0 +1,11 @@
+    #include<stdio.h>
+    int main()
+    {
+      int x;
+      scanf("%d",&x);
+      if(x==0)
+      printf("1\n");
+      else if(x==1)
+         printf("0\n");
+      return 0;
+    }
