781df9fc3f2db46f7e5c8cd7480c539213285131
Zannatul-Naim <zannatulnaim09@gmail.com>
2022-01-17 02:36:43 +0600

    B - A Reverse

--- Atcoder/ABC/ABC-233/B.cpp
+++ Atcoder/ABC/ABC-233/B.cpp
@@ -1,15 +1,20 @@
-#include <iostream>
-#include<cmath>
-using namespace std;
- 
-int main()
-{
-   int l, m;
-   cin >> l >> m;
-   string s;
-   cin >> s;
-   for(int i = 0; i < l - 1; i++)
-        cout << s[i];
-   for(int i = m - 1; i >= l - 1; i--)
-        cout << s[i];
-   for(int i = m; i < s.length(); i++)
+    #include <iostream>
+    #include<cmath>
+    using namespace std;
+     
+    int main()
+    {
+       int l, m;
+       cin >> l >> m;
+       string s;
+       cin >> s;
+       for(int i = 0; i < l - 1; i++)
+            cout << s[i];
+       for(int i = m - 1; i >= l - 1; i--)
+            cout << s[i];
+       for(int i = m; i < s.length(); i++)
+            cout <<s[i];
+       cout << "\n";
+       
+       return 0;
+    }
