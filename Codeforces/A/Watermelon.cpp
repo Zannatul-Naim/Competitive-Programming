41f0e99d305c86fecc1db09874fd59e09d4f6df7
Zannatul-Naim <zannatulnaim09@gmail.com>
2022-01-17 03:26:23 +0600

    Contest-4 => A-Watrmelon

---
+++ Codeforces/A/Watermelon.cpp
@@ -0,0 +1,12 @@
+    #include<stdio.h>
+    int main()
+    {
+        int w;
+        while(scanf("%d",&w)==1){
+            if(w%2==0 && w!=2)
+                printf("YES\n");
+            else
+                printf("NO\n");
+        }
+        return 0;
+    }
