cd3c6807227784210d536c83fb5848810f66ca67
Zannatul-Naim <zannatulnaim09@gmail.com>
2022-01-17 03:30:20 +0600

    Contest 1 => A-Theatre Square

--- Codeforces/A/Theatre_Square.cpp
+++ Codeforces/A/Theatre_Square.cpp
@@ -1,35 +1,29 @@
     #include <bits/stdc++.h>
-    #include <iostream>
-    #include <vector>
-    #include <set>
-    #include <iterator>
-    #include <string>
-    #include <stdio.h>
-    #include <string.h>
-    #include <sstream>
-    #include <ctype.h>
      
     using namespace std;
      
-    int main(){
-        long long x,y,a;
-        cin>>x>>y>>a;
-        long long remx,remy;
-        remx=0;
-        remy=0;
-        if (x%a==0){
-                remx+=x/a;
+    int main()
+    {
+        long long x, y, a;
+        cin >> x >> y >> a;
+        long long remx, remy;
+        remx = 0;
+        remy = 0;
+        if (x%a == 0){
+                remx += x / a;
      
         }
-        else{
-            remx+=1+(x/a);
+        else
+        {
+            remx+ = 1 + (x/a);
         }
-        if (y%a==0){
-            remy+=y/a;
+        if (y % a == 0){
+            remy += y / a;
         }
-        else{
-            remy+=1+(y/a);
+        else
+        {
+            remy += 1 + (y/a);
         }
-        cout<<remx*remy;
+        cout << remx * remy;
     return 0;
     }
