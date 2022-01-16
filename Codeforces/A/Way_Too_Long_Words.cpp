e14ee261d3202d2ea2b0b69bfb3408aa7de71e48
Zannatul-Naim <zannatulnaim09@gmail.com>
2022-01-17 03:32:19 +0600

    contest 71 => A Way Too Long Words

---
+++ Codeforces/A/Way_Too_Long_Words.cpp
@@ -0,0 +1,23 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+    int main()
+    {
+        int n,l;
+        cin>>n;
+     
+        string str;
+        for(int i=1;i<=n;i++){
+     
+                cin>>str;
+                   l=str.size();
+     
+            if(l>10)
+            {
+                cout<<str[0]<< l-2 << str[l-1]<< "\n";
+            }
+            else
+                cout<<str<< "\n";
+        }
+     
+        return 0;
+    }
