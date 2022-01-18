8a53dead8b9186579e3b5ed931b36cc592cad1a4
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-01-18 19:09:08 +0600

    4A - Watermelon

--- Codeforces/A/Watermelon.cpp
+++ Codeforces/A/Watermelon.cpp
@@ -1,12 +1,14 @@
-    #include<stdio.h>
+// Watermelon.cpp
+#include<stdio.h>
     int main()
-    {
+ {
         int w;
-        while(scanf("%d",&w)==1){
+        while(scanf("%d",&w)==1)
+        {
             if(w%2==0 && w!=2)
                 printf("YES\n");
             else
                 printf("NO\n");
         }
         return 0;
-    }
+}
