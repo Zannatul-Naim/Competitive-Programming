e820e6fc44e1d5f9bf631892288acabae41e7d16
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-01-26 10:46:09 +0600

    4A   Watermelon.

--- Codeforces/A/watermelon.c
+++ Codeforces/A/watermelon.c
@@ -1,11 +1,11 @@
-// Watermelon.cpp
+// Watermelon.c
 #include<stdio.h>
     int main()
  {
         int w;
         while(scanf("%d",&w)==1)
         {
-            if(w%2==0 && w!=2)
+            if(w % 2 == 0 && w != 2)
                 printf("YES\n");
             else
                 printf("NO\n");
