e91f404823130baffa3bf3b26eb053079e397fe5
Zannatul-Naim <zannatulnaim09@gmail.com>
2022-02-02 21:03:48 +0600

    Simple Sum

---
+++ URI/Beginner/1003.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     int A,B,SOMA;
+     scanf("%d %d", &A,&B);
+     SOMA=A+B;
+     printf("SOMA = %d\n", SOMA);
+ 
+    return 0;
+}
