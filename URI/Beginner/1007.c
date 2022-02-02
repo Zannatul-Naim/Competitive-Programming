09c20228734d6c8c014977ca7f4f22f9fc7f3ff5
Zannatul-Naim <zannatulnaim09@gmail.com>
2022-02-02 21:05:18 +0600

    Difference

---
+++ URI/Beginner/1007.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,DIFERENCA;
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    DIFERENCA=(a*b-c*d);
+    printf("DIFERENCA = %d\n",DIFERENCA);
+    return 0;
+}
