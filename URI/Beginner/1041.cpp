f55c5ad1edee140141632e485a58b43259646a9e
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-03 17:04:50 +0600

    Coordinates of a Point

---
+++ URI/Beginner/1041.cpp
@@ -0,0 +1,44 @@
+ #include<iostream>
+ using namespace std;
+ int main()
+ {
+     double x,y;
+     cin>>x>>y;
+     if(x==0.0 && y==0.0)
+     {
+         cout<<"Origem"<<"\n";
+     }
+     else if(y==0.0)
+     {
+          cout<<"Eixo X"<<"\n";
+     }
+     else if(x==0.0)
+     {
+          cout<<"Eixo Y"<<"\n";
+     }
+     else if(x>0.0)
+     {
+        if(y>0.0)
+        {
+            cout<<"Q1"<<"\n";
+        }
+        else if(y<0.0)
+        {
+             cout<<"Q4"<<"\n";
+        }
+
+     }
+          else if(x<0.0)
+     {
+        if(y>0.0)
+        {
+            cout<<"Q2"<<"\n";
+        }
+        else if(y<0.0)
+        {
+             cout<<"Q3"<<"\n";
+        }
+
+     }
+     return 0;
+ }
