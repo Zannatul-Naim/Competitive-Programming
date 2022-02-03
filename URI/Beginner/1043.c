13a87b8da4416edd53218131d623f9be81929fca
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-03 17:09:26 +0600

    Triangle

---
+++ URI/Beginner/1043.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    double a,b,c;
+    scanf("%lf %lf %lf", &a,&b,&c);
+    if(a<(b+c)&& b<(a+c) && c<(b+a))
+    {
+        printf("Perimetro = %.1lf\n",(a+b+c));
+       
+    }
+    else
+    {
+     printf("Area = %.1lf\n",((a+b)*c/2));
+    }
+    return 0;
+}
