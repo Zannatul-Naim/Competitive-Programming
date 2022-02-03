6680065359d859682694d45af3c28231f5eb2fa5
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-03 17:03:17 +0600

    Interval

---
+++ URI/Beginner/1037.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    double a;
+    scanf("%lf",&a);
+    if(a<0 || a>100)
+    {
+        printf("Fora de intervalo\n");
+    }
+    else if(a>=0.0 && a<=25.0)
+    {
+        printf("Intervalo [0,25]\n");
+    }
+    else if(a>25.0 && a<=50.0)
+    {
+        printf("Intervalo (25,50]\n");
+    }
+    else if(a>50.0 && a<=75.0)
+    {
+        printf("Intervalo (50,75]\n");
+    }
+     else if(a>75.0 && a<=100.0)
+    {
+        printf("Intervalo (75,100]\n");
+    }
+  return 0;
+
+}
