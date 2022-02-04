d103d7db75e5fd376db08d86cfa60854f6b3bd20
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-04 17:13:37 +0600

    Triangle Types

---
+++ URI/Beginner/1045.c
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#include <iostream>
+
+ 
+using namespace std;
+ 
+int main() {
+        double a,b,c,temp;
+        cin>>a>>b>>c;
+        if(a<b)
+        {
+            temp=a;
+            a=b;
+            b=temp;
+        }
+       if (b < c)
+
+     {
+        temp = b;
+        b = c;
+        c = temp;
+      }
+    if(a<b)
+        {
+            temp=a;
+            a=b;
+            b=temp;
+        }
+        if(a>=(b+c))
+        {
+        cout<<"NAO FORMA TRIANGULO"<<"\n";
+        }
+       else if(a*a==(b*b+c*c))
+        {
+        cout<<"TRIANGULO RETANGULO"<<"\n";
+        }
+        else if(a*a>(b*b+c*c))
+        {
+        cout<<"TRIANGULO OBTUSANGULO"<<"\n";
+        }
+        else if(a*a<(b*b+c*c))
+        {
+        cout<<"TRIANGULO ACUTANGULO"<<"\n";
+        }
+        if(a==b && b==c)
+        {
+        cout<<"TRIANGULO EQUILATERO"<<"\n";
+        }
+        else if(a==b || b==c)
+        {
+        cout<<"TRIANGULO ISOSCELES"<<"\n";
+        }
+    return 0;
+}
