3e8ad5fc7f69683b3b3bccd77f4eeee187c2750a
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-04 17:19:06 +0600

    Month

---
+++ URI/Beginner/1052.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+int n;
+cin>>n;
+if(n==1)
+cout<<"January"<<"\n";
+else if(n==2)
+cout<<"February"<<"\n";
+else if(n==3)
+cout<<"March"<<"\n";
+else if(n==4)
+cout<<"April"<<"\n";
+ else if(n==5)
+cout<<"May"<<"\n";
+else if(n==6)
+cout<<"June"<<"\n";
+else if(n==7)
+cout<<"July"<<"\n"; 
+else if(n==8)
+cout<<"August"<<"\n";
+else if(n==9)
+cout<<"September"<<"\n";
+else if(n==10)
+cout<<"October"<<"\n";
+else if(n==11)
+cout<<"November"<<"\n";
+else if(n==12)
+cout<<"December"<<"\n";
+    return 0;
+}
