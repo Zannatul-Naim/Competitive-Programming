79398987ff413dd0b176b219fa5561c508a56bf4
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-05 15:02:52 +0600

    Sum of Consecutive Odd numbers 1

---
+++ URI/Beginner/1099.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x,y,sum_of_odd=0;
+        cin>>x>>y;
+        if(x<=y)
+        {
+        for(x=x+1;x<y;x++)
+        {
+            if(x%2==1)
+            sum_of_odd+=x;
+
+        }
+        }
+        else if(x>y)
+        {
+        for(y=y+1;y<x;y++)
+        {
+            if(y%2==1)
+            sum_of_odd+=y;
+
+        }
+        }
+        cout<<sum_of_odd<<"\n";
+    }
+    return 0;
+}
