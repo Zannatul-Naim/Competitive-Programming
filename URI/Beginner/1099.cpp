5801e385acf0448f9af99da3fda5331a54e3aae9
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-05 15:04:18 +0600

    Sum of Consecutive Odd Numbers 2

--- URI/Beginner/1099.cpp
+++ URI/Beginner/1099.cpp
@@ -8,21 +8,21 @@ int main()
     {
         int x,y,sum_of_odd=0;
         cin>>x>>y;
-        if(x<=y)
+        if(x <= y)
         {
-        for(x=x+1;x<y;x++)
+        for(x = x+1; x < y; x++)
         {
-            if(x%2==1)
-            sum_of_odd+=x;
+            if(x%2 == 1)
+            sum_of_odd += x;
 
         }
         }
-        else if(x>y)
+        else if(x > y)
         {
-        for(y=y+1;y<x;y++)
+        for(y = y+1; y < x; y++)
         {
-            if(y%2==1)
-            sum_of_odd+=y;
+            if(y%2 == 1)
+            sum_of_odd += y;
 
         }
         }
