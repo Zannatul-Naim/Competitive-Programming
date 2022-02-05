62b74e41dd78860c34edeaea73f1d2e37ec772a1
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-05 14:59:35 +0600

    Sequence IJ 1

---
+++ URI/Beginner/1095.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i=1,j=60;
+    while(j>=0)
+    {
+        cout<<"I="<<i<<" "<<"J="<<j<<"\n";
+        j-=5;
+        i+=3;
+    }
+     return 0;
+}
