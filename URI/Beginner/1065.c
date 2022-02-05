e82bc1ec8bd795dd39e771eb81ac0ece10fa588d
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-05 14:47:37 +0600

    Even Between Five numbers.

---
+++ URI/Beginner/1065.c
@@ -0,0 +1,18 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+ int n,cnt=0;
+ for(int i=0;i<5;i++)
+ {
+     cin>>n;
+     if(n%2==0)
+     {
+         cnt++;
+     }
+ }
+ cout<<cnt<<" valores pares"<<"\n";
+    return 0;
+}
