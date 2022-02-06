06d624776ca029086cd68a93ce52099a6aa1ee12
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-06 22:31:09 +0600

    Squared and Cubic

---
+++ URI/Beginner/1143.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+   long int n;
+   cin>>n;
+   for(int i=1;i<=n;i++)
+   {
+       cout<<i<<" "<<i*i<<" "<<i*i*i<<"\n";
+       //cout<<i<<" "<<i*i +1<<" "<<i*i*i +1<<"\n";
+   }
+    return 0;
+}
