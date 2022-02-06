efd5057738ff18e2bc0090f239ba44ad092cce01
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-06 22:32:57 +0600

    Prime number

---
+++ URI/Beginner/1165.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<math.h>
+int main() {
+     int t;
+     scanf("%d",&t);
+     while(t--)
+     {
+         int cnt=0;
+        long int n;
+        scanf("%ld",&n);
+       for(int i=2;i<=sqrt(n);i++)
+       {
+           if(n%i==0)
+           cnt++;
+       }
+       if(cnt==0)
+       printf("%ld eh primo\n",n);
+       else
+       printf("%ld nao eh primo\n",n);
+     }
+
+    return 0;
+}
