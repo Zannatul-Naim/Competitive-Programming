6a61240a7be497f83547866e6fa61a0df1fe05a2
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-06 22:31:43 +0600

    PUM

---
+++ URI/Beginner/1142.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+     int n,i=1;
+     cin>>n;
+     while(n--)
+     {
+        cout<<i<<" "<<i+1<<" "<<i+2<<" PUM"<<"\n";
+        i=i+4;
+     }
+    return 0;
+}
