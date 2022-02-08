d4ee0f34a068e79937fe333d9a28dc450530e297
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-08 19:57:02 +0600

    Logical Sequence

--- URI/Beginner/1144.cpp
+++ URI/Beginner/1144.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main() {
 
    long int n;
-   cin>>n;
-   for(int i=1;i<=n;i++)
+   cin >> n;
+   for(int i = 1; i <= n; i++)
    {
-       cout<<i<<" "<<i*i<<" "<<i*i*i<<"\n";
-       cout<<i<<" "<<i*i +1<<" "<<i*i*i +1<<"\n";
+       cout<< i << " " << i*i << " " << i*i*i <<"\n";
+       cout<< i << " " << i*i + 1 << " " << i*i*i + 1 <<"\n";
    }
     return 0;
 }
