4524062b745c1e6c8bc3b951666e10c793b872aa
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-09 18:42:04 +0600

    Divide X by Y

--- URI/Beginner/1116.c
+++ URI/Beginner/1116.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main()
 {
-    int a,b,c,d;
+    int a, b, c, d;
     double e;
     scanf("%d", &a);
-    for(b=1; b<=a; b++)
+    for(b = 1; b <= a; b++)
     {
-        scanf("%d%d", &c, &d);
-        if(d==0)
+        scanf("%d %d", &c, &d);
+        if(d == 0)
             printf("divisao impossivel\n");
         else
         {
-            e=c/(d*1.00);
+            e = c / (d * 1.00);
             printf("%.1lf\n", e);
         }
     }
