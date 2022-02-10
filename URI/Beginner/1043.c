e5b1022895beebec8d3b1e9295b10d863b863ad6
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-02-10 23:52:46 +0600

    Triangle

--- URI/Beginner/1043.c
+++ URI/Beginner/1043.c
@@ -3,8 +3,8 @@
 int main()
 {
     double a,b,c;
-    scanf("%lf %lf %lf", &a,&b,&c);
-    if(a<(b+c)&& b<(a+c) && c<(b+a))
+    scanf("%lf %lf %lf", &a, &b, &c);
+    if(a < (b+c) && b < (a+c) && c < (b+a))
     {
         printf("Perimetro = %.1lf\n",(a+b+c));
        
