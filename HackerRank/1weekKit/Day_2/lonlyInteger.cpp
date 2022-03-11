b0a50c7fda14238153501b2f741f996e2b6c2ad6
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-03-11 10:46:57 +0600

    Lonely Integer
    Given an array of integers, where all elements but one occur twice, find the unique element.

--- HackerRank/1weekKit/Day_2/lonlyInteger.cpp
+++ HackerRank/1weekKit/Day_2/lonlyInteger.cpp
@@ -1,1 +1,29 @@
-Updating,........
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+   int n;
+   cin >> n;
+   int a[n];
+   for(int i = 0; i < n; i++) {
+           cin >> a[i];
+   }
+   sort(a, a+n);
+   for(int i = 0; i < n; i += 2)
+   {
+       if(a[i] == a[i+1]) continue;
+       else {
+           cout << a[i] << endl;
+           break;
+       }
+   }
+    
+    return 0;
+} 
