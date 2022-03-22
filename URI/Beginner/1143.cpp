687690779a4d1e13bf270e1efd1a431bbba35e19
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-03-22 22:45:07 +0600

    Squared and cubic

--- URI/Beginner/1143.cpp
+++ URI/Beginner/1143.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main() {
 
    long int n;
-   cin>>n;
-   for(int i=1;i<=n;i++)
+   cinn >> n;
+   for(int i = 1; i <= n; i++)
    {
-       cout<<i<<" "<<i*i<<" "<<i*i*i<<"\n";
+       cout << i << " " << i*i << " " << i*i*i <<"\n";
        //cout<<i<<" "<<i*i +1<<" "<<i*i*i +1<<"\n";
    }
     return 0;
