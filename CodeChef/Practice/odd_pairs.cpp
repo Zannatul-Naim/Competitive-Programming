47b6ceb721e6c77f3192474f3df334e4b7e9f36b
Zannatul Naim <zannatulnaim09@gmail.com>
2022-08-25 22:25:04 +0600

    Code : ODDPAIRS

---
+++ CodeChef/Practice/odd_pairs.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+
+int gcd(int a, int b) {
+	return b==0? a : gcd(b, a%b);
+}
+
+int lcm(int a, int b) {
+	return (a * (b / gcd(a, b)));
+}
+
+bool isPrime(int n) {  // A number is Prime number or not.
+	if(n < 2) return false;
+	for(int i = 0; i*i <= n; i++) {
+		if(n % i == 0) return false;
+	}
+	return true;
+}
+
+long long int factorial(int n) {
+	return n * factorial(n-1);
+}
+
+ll pow(int a, int n) {
+	if(n == 0) return 1;
+	if(n == 1) return a;
+	ll t = pow(a, n/2);
+	return t*t*pow(a, n%2);
+}
+
+template<class T>
+void swap(T &a, T &b) {
+	T temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+void solve() {
+  ll n;
+  cin >> n;
+  ll ans = (n*n) / 2;
+  cout << ans << endl;
+  }
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+	
+	int t;
+	cin >> t;
+	while(t--) {
+		solve();
+	}
+
+	return 0;
+}
