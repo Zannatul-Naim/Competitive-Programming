ecdf4969d844a9482d913cd168ef22686ae58c93
Zannatul Naim <zannatulnaim09@gmail.com>
2023-02-17 01:32:56 +0600

    1793A Yet another promotion

---
+++ Codeforces/yet_another_promotion.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define input cin >>
+#define output cout <<
+#define pb push_back
+
+void fastIO() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+int gcd(int a, int b) {
+	return b==0? a : gcd(b, a%b);
+}
+
+int lcm(int a, int b) {
+	return (a * (b / gcd(a, b)));
+}
+
+bool isPrime(int n) { 
+	if(n < 2) return false;
+	for(int i = 2; i*i <= n; i++) {
+		if(n % i == 0) return false;
+	}
+	return true;
+}
+
+void solve() {
+
+	ll a, b;
+	cin >> a >> b;
+
+	ll n, m;
+	cin >> n >> m;
+
+	ll ans1 = b*n;
+	ll x = m+1;
+	ll ans2 = (n/x) * m * a;
+	ans2 += (n%x) * min(a, b);
+
+	cout << min(ans1, ans2) << endl;
+
+}
+
+int main()
+{
+	fastIO();
+
+	int t = 1;
+	cin >> t;
+
+	while(t--) {
+	  solve();
+	}
+
+    // cerr << "Execution time: " << 1.0 * clock() / CLOCKS_PER_SEC << "s.\n";
+
+    return 0;
+}
