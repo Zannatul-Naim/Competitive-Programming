1f83d414c0fb96b0010235b066bc4aa5075bddc2
Zannatul Naim <zannatulnaim09@gmail.com>
2023-02-20 09:01:14 +0600

    Create 709. To Lower Case.cpp

---
+++ Leetcode/709. To Lower Case.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] >= 'A' && s[i] <= 'Z') s[i] = s[i] + 'a' - 'A';
+        }
+
+        return s;
+    }
+};
