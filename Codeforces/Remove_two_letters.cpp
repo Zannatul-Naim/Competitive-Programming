0a23a4c26067444742d7f165851b7f9240831f62
Zannatul Naim <zannatulnaim09@gmail.com>
2023-03-03 00:25:22 +0600

    Create Remove_two_letters.cpp

---
+++ Codeforces/Remove_two_letters.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define pb push_back
+#define sortAll sort(v.begin(), v.end());
+
+
+void fastIO() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+void solve() {
+
+	ll n;
+	cin >> n;
+
+	string s;
+	cin >> s;
+
+	int cnt = 1;
+
+	for(int i = 0; i < n-2; i++) {
+		if(s[i] != s[i+2]) cnt++;
+	}
+
+	cout << cnt << endl;
+
+}
+
+int main()
+{
+	fastIO();
+
+	int t = 1;
+	cin >> t;
+
+	while(t--) {
+	  solve();
+	}
+
+    // cerr << "Execution time: " << 1.0 * clock() / CLOCKS_PER_SEC << "s.\n";
+
+    return 0;
+}
