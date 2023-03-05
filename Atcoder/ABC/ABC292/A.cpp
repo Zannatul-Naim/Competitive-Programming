67c0473c9921f7f492ef1b0126c638f36df1a3c6
Zannatul Naim <zannatulnaim09@gmail.com>
2023-03-06 00:03:13 +0600

    Create A.cpp

---
+++ Atcoder/ABC/ABC292/A.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define pb push_back
+#define sortAll sort(v.begin(), v.end());
+
+
+void fastIO() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+void solve() {
+
+	string s;
+	cin >> s;
+
+	for(auto i : s) {
+		cout << char(i - 'a' + 'A');
+	} cout << endl;
+
+}
+
+int main()
+{
+	fastIO();
+
+	int t = 1;
+	// cin >> t;
+
+	while(t--) {
+	  solve();
+	}
+
+    // cerr << "Execution time: " << 1.0 * clock() / CLOCKS_PER_SEC << "s.\n";
+
+    return 0;
+}
