85e6a9c870c28034688586feefa3612a3e9f335a
Zannatul Naim <zannatulnaim09@gmail.com>
2023-04-09 23:44:20 +0600

    Create Ian_visits_mary.cpp

---
+++ Codeforces/Ian_visits_mary.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define in cin >>
+#define out cout <<
+#define pb push_back
+
+void fastIO() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+
+void solve() {
+
+	int a, b;
+	cin >> a >> b;
+
+	cout << 2 << endl;
+	cout << 1 << " " << b-1 << endl;
+	cout << a << " " << b << endl;
+	
+}
+
+int main()
+{
+	fastIO();
+
+	int t = 1;
+	cin >> t;
+
+	while(t--) {
+	  solve();
+	}
+
+	return 0;
+}
