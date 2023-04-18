4ad57569e6ba7ee05cd3f9408afa941b2ca8e26b
Zannatul Naim <zannatulnaim09@gmail.com>
2023-04-18 21:02:40 +0600

    Create required_remainder.cpp

---
+++ Codeforces/required_remainder.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define in cin >>
+#define out cout <<
+#define pb push_back
+
+void fastIO() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+
+void solve() {
+
+	int x, y, n;
+	cin >> x >> y >> n;
+
+	if(x > n) {
+		cout << "0" << endl;
+		return;
+	}
+
+	int k = n - n%x ;
+
+	if(k+y <= n) {
+		cout << k+y << endl;
+		return;
+	}
+	cout << k - (x-y) << endl;
+	return;
+
+}	
+
+int main()
+{
+	fastIO();
+
+	int t = 1;
+	cin >> t;
+
+	while(t--) {
+	  solve();
+	}
+
+	return 0;
+}
