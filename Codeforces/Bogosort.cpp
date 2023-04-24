c5218474a853f76b79466bc8f13bfd8d5f73a9dc
Zannatul Naim <zannatulnaim09@gmail.com>
2023-04-24 23:04:32 +0600

    Create Bogosort.cpp

---
+++ Codeforces/Bogosort.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define in cin >>
+#define out cout <<
+#define pb push_back
+
+void fastIO() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+
+void solve() {
+
+	int n;
+	cin >> n;
+
+	vector < int > v(n);
+
+	for(auto &i : v) cin >> i;
+
+	sort(v.rbegin(), v.rend());
+
+	for(auto i : v) cout << i << " ";
+	cout << endl;
+
+}	
+
+int main()
+{
+	fastIO();
+
+	int t = 1;
+	cin >> t;
+
+	while(t--) {
+	  solve();
+	}
+
+	return 0;
+}
