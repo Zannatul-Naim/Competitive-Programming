0ce1d7d409484762602ed8b76b9d2efcc5d65b5a
Zannatul Naim <zannatulnaim09@gmail.com>
2023-04-29 21:24:24 +0600

    Create Bun_lover.cpp

---
+++ Codeforces/Bun_lover.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define in cin >>
+#define out cout <<
+#define pb push_back
+
+void fastIO() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+
+void solve() {
+
+	ll n;
+	cin >> n;
+	ll ans = (n+1)*(n+1);
+	cout << ans+1 << endl;
+
+}	
+
+int main()
+{
+	fastIO();
+
+	int t = 1;
+	cin >> t;
+
+	while(t--) {
+	  solve();
+	}
+
+	return 0;
+}
