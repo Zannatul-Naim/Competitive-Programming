84fd56ee96d556e5498a90438d6c248816a6340d
Zannatul Naim <zannatulnaim09@gmail.com>
2023-05-04 17:09:33 +0600

    Create Optimal_reduction.cpp

---
+++ Codeforces/Optimal_reduction.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define in cin >>
+#define out cout <<
+#define pb push_back
+
+void fastIO() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+
+void solve() {
+
+	int n;
+	cin >> n;
+	int a[n];
+
+	for(int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	bool f1 = false, f2 = false;
+
+	for(int i = 0; i < n-1; i++) {
+		if(a[i] > a[i+1]) f1 = true;
+		if(a[i] < a[i+1] and f1) {
+			cout << "NO" << endl;
+			return;
+		}
+	}
+
+	cout << "YES" << endl;
+	return;
+
+}	
+
+int main()
+{
+	fastIO();
+
+	int t = 1;
+	cin >> t;
+
+	while(t--) {
+	  solve();
+	}
+
+	return 0;
+}
