7c6574498fd79835b1c9320ce67b7155ba8e62f7
Zannatul Naim <zannatulnaim09@gmail.com>
2023-05-23 09:34:48 +0600

    Create balancec_array.cpp

---
+++ Codeforces/balancec_array.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define in cin >>
+#define out cout <<
+#define pb push_back
+#define YES 1
+#define NO 0
+
+void fastIO() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+void print(bool f) {
+	if(f) cout << "YES" << endl;
+	else cout << "NO" << endl;
+}
+
+void solve() {
+
+	int n;
+	cin >> n;
+
+	n /= 2;
+
+	if(n&1) {
+		cout << "NO" << endl;
+		return;
+	}
+
+	cout << "YES" << endl;
+	for(int i = 1; i <= n; i++) {
+		cout << i*2 << " ";
+	}
+	for(int i = 1; i < n; i++) {
+		cout << 2*i-1 << " ";
+	}
+	cout << 3*n-1 << endl;
+
+}	
+
+int main()
+{
+	fastIO();
+
+	int t = 1;
+	cin >> t;
+
+	while(t--) {
+	  solve();
+	}
+
+	return 0;
+}
