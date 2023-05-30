1788a0e71041961a26062ba93e37ec65527dcc3e
Zannatul Naim <zannatulnaim09@gmail.com>
2023-05-30 11:03:40 +0600

    Create Grasshopper_on_a_line.cpp

---
+++ Codeforces/Grasshopper_on_a_line.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define in cin >>
+#define out cout <<
+#define pb push_back
+#define YES 1
+#define NO 0
+
+void fastIO() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+void print(bool f) {
+	if(f) cout << "YES" << endl;
+	else cout << "NO" << endl;
+}
+
+void solve() {
+
+	ll x, k;
+	cin >> x >> k;
+	
+	if(x%k == 0) {
+		cout << 2 << endl;
+		cout << x-1 << " " << 1 << endl;
+		return;
+	}
+	
+	cout << 1 << endl;
+	cout << x << endl;
+
+}	
+
+int main()
+{
+	fastIO();
+
+	int t = 1;
+	cin >> t;
+
+	while(t--) {
+	  solve();
+	}
+
+	return 0;
+}
