5d1797321bdc3a85c9fd6ed4d154b91873a6b03a
Zannatul Naim <zannatulnaim09@gmail.com>
2023-06-29 21:26:01 +0600

    Extra code deleted

--- Codeforces/Find_Extra_One.cpp
+++ Codeforces/Find_Extra_One.cpp
@@ -10,74 +10,20 @@ void fastIO() {
 	cout.tie(0);
 }
 
-int gcd(int a, int b) {
-	return b==0? a : gcd(b, a%b);
-}
-
-int lcm(int a, int b) {
-	return (a * (b / gcd(a, b)));
-}
-
-bool isPrime(int n) {  // A number is Prime number or not.
-	if(n < 2) return false;
-	for(int i = 2; i*i <= n; i++) {
-		if(n % i == 0) return false;
-	}
-	return true;
-}
-
-long long int factorial(int n) {
-	return n * factorial(n-1);
-}
-
-ll pow(int a, int n) {
-	if(n == 0) return 1;
-	if(n == 1) return a;
-	ll t = pow(a, n/2);
-	return t*t*pow(a, n%2);
-}
-
-template<class T>
-void swap(T &a, T &b) {
-	T temp;
-	temp = a;
-	a = b;
-	b = temp;
-}
-
-bool checkIsDistinct(int n) {
-  int digit = log10(n) + 1;
-  set<int>s;
-  while(n) {
-    s.insert(n%10);
-    n /= 10;
-  }
-  if(s.size() == digit) return true;
-  else return false;
-}
-
-// void solve() {
-  
-// }
-
 int main()
 {
 	fastIO();
-
-	// freopen("input.txt", 'r');
-	// freopen("output.txt", 'w');
-
+	
 	int t;
 	int left = 0, right = 0;
 	
 	cin >> t;
 	while(t--) {
-		int a, b;
-    cin >> a >> b;
+	int a, b;
+   	 cin >> a >> b;
     
-    if(a > 0 ) right++;
-    else if(a < 0) left++;
-     
+        if(a > 0 ) right++;
+        else if(a < 0) left++;
 	}
 	if(left == 1 || right == 1 || left == 0 || right == 0) cout << "YES" << endl;
 	else cout << "NO" << endl;
